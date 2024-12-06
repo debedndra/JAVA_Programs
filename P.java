2562e9aa63c6357e9f077a405026532b1667c2ef
debedndra <149603756+debedndra@users.noreply.github.com>
2024-12-06 11:38:32 +0530

    Add files via upload

---
+++ P.java
@@ -0,0 +1,14 @@
+public class  P
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main of class P");
+	}
+}
+class Q 
+{
+	public static void main ()
+	{
+		System.out.println("main of class Q");
+	}
+}
---
+++ X.java
@@ -0,0 +1,16 @@
+public class X
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main of class X");
+	}
+}
+class Y
+ {
+	public static void main()
+	{
+		System.out.println("main of class Y");
+
+	}
+
+}
